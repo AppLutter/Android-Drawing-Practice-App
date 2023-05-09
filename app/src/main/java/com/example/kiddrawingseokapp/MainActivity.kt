aac7b04fc6a3c351c90ecf7fcf9ce5f135d65371
HanSeokMin <tjrals604@naver.com>
2023-05-09 11:26:19 +0900

    색상 선택해서 그리기까지 완료

--- app/src/main/java/com/example/kiddrawingseokapp/MainActivity.kt
+++ app/src/main/java/com/example/kiddrawingseokapp/MainActivity.kt
@@ -3,8 +3,10 @@ package com.example.kiddrawingseokapp
 import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.ImageButton
 import android.widget.LinearLayout
+import android.widget.Toast
 import androidx.core.content.ContextCompat
 import androidx.core.view.get
 
@@ -22,7 +24,7 @@ class MainActivity : AppCompatActivity() {
         drawingView?.setSizeForBrush(20.toFloat())
 
         val linearLayoutPaintColors = findViewById<LinearLayout>(R.id.ll_paint_colors)
-        mImageButtonCurrentPaint = linearLayoutPaintColors[1] as ImageButton
+        mImageButtonCurrentPaint = linearLayoutPaintColors[0] as ImageButton
         mImageButtonCurrentPaint!!.setImageDrawable(
             ContextCompat.getDrawable(this, R.drawable.palette_pressed)
         )
@@ -57,6 +59,24 @@ class MainActivity : AppCompatActivity() {
         }
 
         brushDialog.show()
+    }
+
+    fun paintClicked(view: View) {
+        if (view !== mImageButtonCurrentPaint) {
+            val imageButton = view as ImageButton
+            // tag가 가리키는 실제 resource값을 읽어옴
+            val colorTag = imageButton.tag.toString()
+            drawingView?.setColor(colorTag)
+            view.setImageDrawable(
+                ContextCompat.getDrawable(this, R.drawable.palette_pressed)
+            )
+
+            mImageButtonCurrentPaint?.setImageDrawable(
+                ContextCompat.getDrawable(this, R.drawable.palette_normal)
+            )
+
+            mImageButtonCurrentPaint = view
+        }
     }
 }
 
