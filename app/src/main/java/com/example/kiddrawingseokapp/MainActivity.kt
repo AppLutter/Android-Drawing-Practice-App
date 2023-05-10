e598f2670337d884e1184c660b49f9696bcab592
HanSeokMin <tjrals604@naver.com>
2023-05-10 09:39:27 +0900

    커스텀 다이얼로그 완성

--- app/src/main/java/com/example/kiddrawingseokapp/MainActivity.kt
+++ app/src/main/java/com/example/kiddrawingseokapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.kiddrawingseokapp
 
+import android.Manifest
 import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -7,6 +8,10 @@ import android.view.View
 import android.widget.ImageButton
 import android.widget.LinearLayout
 import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.core.view.get
 
@@ -14,6 +19,27 @@ import androidx.core.view.get
 class MainActivity : AppCompatActivity() {
     private var drawingView: DrawingView? = null
     private var mImageButtonCurrentPaint: ImageButton? = null
+    val requestPermission: ActivityResultLauncher<Array<String>> =
+        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
+            permissions.entries.forEach {
+                val permissionName = it.key
+                val isGranted = it.value
+
+                if (isGranted) {
+                    Toast.makeText(
+                        this@MainActivity,
+                        "파일 읽기 권한 승인 완료", Toast.LENGTH_LONG
+                    ).show()
+                } else {
+                    if (permissionName == Manifest.permission.READ_EXTERNAL_STORAGE) {
+                        Toast.makeText(
+                            this@MainActivity,
+                            "파일 읽기를 거절하셧씁니다.", Toast.LENGTH_LONG
+                        ).show()
+                    }
+                }
+            }
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -29,11 +55,28 @@ class MainActivity : AppCompatActivity() {
             ContextCompat.getDrawable(this, R.drawable.palette_pressed)
         )
 
-        val ib_brush: ImageButton = findViewById<ImageButton>(R.id.ib_brush)
-        ib_brush.setOnClickListener {
+        val ibBrush: ImageButton = findViewById<ImageButton>(R.id.ib_brush)
+        ibBrush.setOnClickListener {
             showBrushSizeChooserDialog()
         }
 
+        val ibGallery: ImageButton = findViewById(R.id.ib_gallery)
+        ibGallery.setOnClickListener {
+            requestStoragePermission()
+        }
+
+    }
+
+    private fun requestStoragePermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(
+                this,
+                Manifest.permission.READ_EXTERNAL_STORAGE
+            )
+        ) {
+            showRationaleDialog("제목입니다", "storage를 거절하셨습니다")
+        } else {
+            requestPermission.launch(arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE))
+        }
     }
 
     private fun showBrushSizeChooserDialog() {
@@ -77,6 +120,15 @@ class MainActivity : AppCompatActivity() {
 
             mImageButtonCurrentPaint = view
         }
+    }
+
+    private fun showRationaleDialog(title: String, message: String) {
+        val builder: AlertDialog.Builder = AlertDialog.Builder(this)
+        builder.setTitle(title).setMessage(message).setPositiveButton("취소") { dialog, _ ->
+            dialog.dismiss()
+        }
+        builder.create().show()
+
     }
 }
 
